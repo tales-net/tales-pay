3b0ac13579853635dc764bf40aa751fc2c58e9ae
Tales Market <tales.net2019@gmail.com>
2026-08-10 05:45:10 +0300

    Update mikrotik.js

--- mikrotik.js
+++ mikrotik.js
@@ -14,7 +14,7 @@ async function disableUserQueue(username) {
         host: process.env.MIKROTIK_HOST,
         user: process.env.MIKROTIK_USER,
         password: process.env.MIKROTIK_PASSWORD,
-        port: parseInt(process.env.MIKROTIK_PORT || '8728'),
+        port: parseInt(process.env.PORT || '3333'),
         timeout: 10
     });
 
