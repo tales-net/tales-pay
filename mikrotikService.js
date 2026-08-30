ca04a74209e3a9dc3667162a9992fa599ff7bc39
Tales Market <tales.net2019@gmail.com>
2026-08-30 07:13:17 +0300

    Update mikrotikService.js

--- mikrotikService.js
+++ mikrotikService.js
@@ -26,17 +26,17 @@ function getCardPrefixAndType(amount) {
   const numAmount = Number(amount);
   switch (numAmount) {
     case 5:
-      return { prefix: "01", profile: "Bronze", packageName: "الباقة البرونزية", isCustom: false };
+      return { prefix: "01", profileId: "4", packageName: "الباقة البرونزية", isCustom: false };   // Bronze ID = 4
     case 15:
-      return { prefix: "02", profile: "Silver", packageName: "الباقة الفضية", isCustom: false };
+      return { prefix: "02", profileId: "5", packageName: "الباقة الفضية", isCustom: false };    // Silver ID = 5
     case 30:
-      return { prefix: "05", profile: "Gold", packageName: "الباقة الذهبية", isCustom: false };
+      return { prefix: "05", profileId: "6", packageName: "الباقة الذهبية", isCustom: false };    // Gold ID = 6
     case 50:
-      return { prefix: "10", profile: "Platinum", packageName: "الباقة البلاتينية", isCustom: false };
+      return { prefix: "10", profileId: "7", packageName: "الباقة البلاتينية", isCustom: false }; // Platinum ID = 7
     case 100:
-      return { prefix: "25", profile: "Diamond", packageName: "الباقة الماسية", isCustom: false };
+      return { prefix: "25", profileId: "8", packageName: "الباقة الماسية", isCustom: false };   // Diamond ID = 8
     default:
-      return { prefix: "", profile: "", packageName: "", isCustom: true };
+      return { prefix: "", profileId: "", packageName: "", isCustom: true };
   }
 }
 
