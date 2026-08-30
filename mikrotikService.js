fed4ba5dece2bdd7be4eb4aae1fd7898cf621505
Tales Market <tales.net2019@gmail.com>
2026-08-30 04:42:05 +0300

    Update mikrotikService.js

--- mikrotikService.js
+++ mikrotikService.js
@@ -25,7 +25,7 @@ function getCardPrefixAndType(amount) {
   const numAmount = Number(amount);
   switch (numAmount) {
     case 5:
-      return { prefix: "01", profile: "Bronze", packageName: "الباقة البرونزية", isCustom: false };
+      return { prefix: "01", profile: "Bronze", packageName: "Bronze", isCustom: false };
     case 15:
       return { prefix: "02", profile: "Silver", packageName: "الباقة الفضية", isCustom: false };
     case 30:
