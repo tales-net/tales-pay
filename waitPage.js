4eaa02c221e1a8d10a5ca1a627e9f4fbf96bf39e
Tales Market <tales.net2019@gmail.com>
2026-09-06 09:35:06 +0300

    Update waitPage.js

--- waitPage.js
+++ waitPage.js
@@ -11,6 +11,7 @@ function generateWaitPageHtml(transactionId, networkUrl) {
       <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="protection.js" defer></script>
         <title>جاري تقديم الطلب - شبكة حكايات</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <style>
