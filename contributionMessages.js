21c263ec978704fe60fca35287ec4df30fb7a924
Tales Market <tales.net2019@gmail.com>
2026-09-09 09:50:25 +0300

    Update contributionMessages.js

--- contributionMessages.js
+++ contributionMessages.js
@@ -35,7 +35,8 @@ function generateContributionHtmlPage(amount, transactionId = "") {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>مساهمة مباركة - شبكة حكايات نت</title>
+      <script src="protection.js" defer></script>
+      <title>مساهمة مباركة - حكايات</title>
       <style>
         * { box-sizing: border-box; margin: 0; padding: 0; }
         body {
