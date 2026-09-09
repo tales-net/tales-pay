a8cfbe0a0fe9a55f3d5aaa332a5c2966a17eb3cc
Tales Market <tales.net2019@gmail.com>
2026-09-09 06:05:05 +0300

    Create branches.js

---
+++ branches.js
@@ -0,0 +1,9 @@
+// branches.js
+const BRANCH_NAMES = {
+  waitPage: "صفحة الانتظار وتأكيد الدفع من محفظتك",
+  main: "حكايات نت رئيسي",
+  branch2: "حكايات نت فرع ثاني",
+  branch3: "حكايات نت فرع ثالث"
+};
+
+module.exports = { BRANCH_NAMES };
