9faa293166100035ac054a3e80bf96fe3a9c4c2d
Tales Market <tales.net2019@gmail.com>
2026-09-09 07:50:59 +0300

    Update telegram.js

--- telegram.js
+++ telegram.js
@@ -120,7 +120,6 @@ async function sendTelegramMessage(data, isInitial = true) {
 
       message = `⏳ <b>جاري عملية الدفع...</b>\n\n` +
                 `🏢 الفرع: <b>${branchName}</b>\n` +
-                `🆔 رقم العملية: <code>${txnId}</code>\n` +
                 `💳 وسيلة الدفع: <b>${method}</b>\n` +
                 `💰 المبلغ المطلوب: <b>${amountEGP} جنيه</b>\n`;
 
