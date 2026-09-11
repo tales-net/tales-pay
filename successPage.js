cce108b3de5f69fc8337282d53ff96a1b4a04df3
Tales Market <tales.net2019@gmail.com>
2026-09-11 05:11:49 +0300

    Update successPage.js

--- successPage.js
+++ successPage.js
@@ -108,7 +108,7 @@ function generateSuccessPageHtml(transactionId, networkUrl, queryBranch) {
               if (attempts < maxAttempts) {
                 setTimeout(pollVoucher, 2000); // الفحص كل ثانيتين
               } else {
-                // إعادة التوجيه إلى صفحة الفشل بعد انقضاء الـ 5 دقائق كاملة
+                // إعادة التوجيه إلى صفحة الفشل بعد انقضاء الـ 3 دقائق كاملة
                 const errorMsg = encodeURIComponent("⚠️ انتهت مهلة الانتظار ولم يتم الدفع بنجاح. تواصل مع الدعم أذا تم الدفع برقم المعاملة: " + txId);
                 window.location.href = '/fail?error=' + errorMsg;
               }
